1c7801968c054ae5c274dc1387f3052040264698
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-17 12:37:10 +0300

    add typed redux hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,6 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
 export const store = configureStore({ reducer: combineReducers(rootReducer) });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
