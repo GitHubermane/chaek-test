47ce26bebcfb7e7b710ec9d3b04414bc23215d2d
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-18 08:23:03 +0300

    add Messages to GamePage

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,19 +1,11 @@
 import { Container, Input, StatusBar } from "components";
-import { useState } from "react";
+import { useAppSelector, useTimer } from "hooks";
 import { timeFormatter } from "utils";
+import { Messages } from "./Messages";
 
 export const GamePage = () => {
-  const timer = () => {
-    const [seconds, setSeconds] = useState(120);
-
-    setTimeout(() => {
-      setSeconds(s => s - 1);
-    }, 1000);
-
-    return seconds;
-  };
-
-  const currentTimer = timer();
+  const { messages } = useAppSelector(state => state.messages);
+  const currentTimer = useTimer();
 
   const percentage = (currentTimer / 120) * 100;
 
@@ -26,11 +18,25 @@ export const GamePage = () => {
         </div>
         <StatusBar percentage={percentage} />
         <div className="p-4">
-          <div className="h-80 relative">
-            <span className="m-auto absolute bottom-2/4 translate-y-2/4 w-full text-center text-gray-400 ">
-              Первый участник вспоминает города...
-            </span>
+          <div className="h-80 relative flex flex-col overflow-auto">
+            {messages.length !== 0 ? (
+              messages.map(message => (
+                <Messages
+                  key={message.id}
+                  {...message}
+                />
+              ))
+            ) : (
+              <span className="m-auto absolute bottom-2/4 translate-y-2/4 w-full text-center text-gray-400 ">
+                Первый участник вспоминает города...
+              </span>
+            )}
           </div>
+          {messages.length > 1 && (
+            <div className="py-2 text-gray-400 text-center">
+              Всего перечислено городов: {messages.length}
+            </div>
+          )}
           <Input placeholder="Ожидаем ответа соперника..." />
         </div>
       </div>
