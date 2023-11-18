0ae40a79910435d04e417eb09d37d412080e4e27
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-18 08:22:05 +0300

    add getIsMyTurn n getLastLetter selectors

--- src/store/messages/selectors.ts
+++ src/store/messages/selectors.ts
@@ -6,3 +6,22 @@ export const getLastMessage = (state: RootState) => {
 
   return messages[lastIndex];
 };
+
+export const getIsMyTurn = (state: RootState) => {
+  const lastMessage = getLastMessage(state);
+
+  return !lastMessage.isMine;
+};
+
+export const getLastLetter = (state: RootState) => {
+  const lastMessage = getLastMessage(state);
+  const lastMessageText = lastMessage.text;
+
+  let lastLetter = lastMessageText[lastMessageText.length - 1];
+
+  if (lastLetter === "ь" || lastLetter === "ъ") {
+    lastLetter = lastMessageText[lastMessageText.length - 2];
+  }
+
+  return lastLetter;
+};
